e8f3f6e5efcb7db46c8eb0fb320e76dd83245868
Wilbur Suero <wilbur.himself@gmail.com>
2019-04-28 23:59:57 -0400

    feat(data frame modification): complete first exercise, using lambas

--- series.py
+++ series.py
@@ -1,9 +1,14 @@
 from __future__ import print_function
 import pandas as pd
+import numpy as np
 
 city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
 population = pd.Series([852469, 1015785, 485199])
 
 cities = pd.DataFrame({ 'City name': city_names, 'Population': population })
-print(cities['City name'])
+cities['Area square miles'] = pd.Series([46.87, 176.53, 97.92])
+cities['Population density'] = cities['Population'] / cities['Area square miles']
+
+cities['Is wide and has sain name'] = (cities['Area square miles'] > 50) & (cities['City name'].apply(lambda name: name.startswith('San')))
+print(cities)
 
